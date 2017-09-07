5813104ab575cdf285c434598a24bfa4efce83e5
Rudy Sanchez <rsanchez@metrostudy.com>
2017-09-07 17:26:13 -0500

    custom rudy changes

--- src/dpDataTable.js
+++ src/dpDataTable.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import map from 'lodash/map';
 import kebabCase from 'lodash/kebabCase';
 import ceil from 'lodash/ceil';
@@ -31,17 +32,26 @@ class DpDataTable extends Component {
       filterableFields: PropTypes.array,
       sortableFields: PropTypes.array,
       defaultSort: PropTypes.string,
+      id: PropTypes.string,
       onEditing: PropTypes.func,
       onDeleting: PropTypes.func,
       itemsPerPage: PropTypes.number,
       showFilter: PropTypes.bool,
       showSort: PropTypes.bool,
       hidePagination: PropTypes.bool,
+      showFooterPagination: PropTypes.bool,
+      showActionCol: PropTypes.bool,
+      tableHeight: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+      ]),
+      tableClass: PropTypes.string,
       isLoading: PropTypes.bool,
       iconClasses: PropTypes.object,
       onSorting: PropTypes.func,
       onFiltering: PropTypes.func,
-      onItemsChange: PropTypes.func
+      onItemsChange: PropTypes.func,
+      onRowClick: PropTypes.func
     };
   }
 
@@ -58,83 +68,84 @@ class DpDataTable extends Component {
   }
 
   render() {
-    const {hidePagination, showFilter, isLoading, iconClasses} = this.props;
+    const { hidePagination, showFooterPagination, showFilter, isLoading, iconClasses, tableHeight, tableClass, id } = this.props;
     const filteredItems = this._filterItems();
-    const icons = extend({'LOADING': 'glyphicon glyphicon-refresh'}, iconClasses);
+    const icons = extend({ 'LOADING': 'fa fa-refresh' }, iconClasses);
     return (
       <div className="row data-table-component" data-toggle="table">
         {isLoading && (<div className="isLoading-container"><i className={icons.LOADING}></i></div>)}
         {!isLoading && showFilter && this._renderFilter()}
         {!isLoading && !hidePagination && this._renderHeaderPagination(filteredItems)}
-        {!isLoading && (<div className="table-responsive col-xs-12">
-          <table className="table table-bordered table-striped">
+        {!isLoading && (<div className="table-responsive col-xs-12" style={{ height: tableHeight ? tableHeight : '225px' }}>
+          <table id={id} className={`table table-bordered table-striped ${tableClass}`}>
             {this._renderHeader()}
             <tbody>
-            {this._renderRow(filteredItems)}
+              {this._renderRow(filteredItems)}
             </tbody>
           </table>
         </div>)}
-        {!isLoading && !hidePagination && this._renderFooterPagination(filteredItems)}
+        {!isLoading && !hidePagination && showFooterPagination && this._renderFooterPagination(filteredItems)}
       </div>
     );
   }
 
   _renderHeader() {
-    const {headers, items, showSort, sortableFields, iconClasses} = this.props;
+    const { headers, items, showSort, sortableFields, iconClasses, showActionCol } = this.props;
     const sortClasses = extend({
-      'SORT_NONE': 'glyphicon glyphicon-sort',
-      'SORT_ASC': 'glyphicon glyphicon-sort-by-attributes',
-      'SORT_DESC': 'glyphicon glyphicon-sort-by-attributes-alt'
+      'SORT_NONE': 'fa fa-sort',
+      'SORT_ASC': 'fa fa-sort-asc',
+      'SORT_DESC': 'fa fa-sort-desc'
     }, iconClasses);
-    const {sortKey, sortOrder} = this.state;
+    const { sortKey, sortOrder } = this.state;
     const itemHeaders = size(headers) > 0 ? headers : keys(items[0]);
     return (
       <thead className={'c' + size(headers)}>
-      <tr>
-        {map(itemHeaders, (header, headerKey) => {
-          if (isNumber(headerKey)) {
-            headerKey = header;
-          }
-          const isSortable = showSort && (size(sortableFields) === 0 || includes(sortableFields, headerKey));
-          const solSortOrder = headerKey === sortKey ? sortOrder : 'NONE';
-          return (
-            <th key={'header-' + kebabCase(headerKey)} data-sortable={isSortable} data-sort-key={headerKey}
+        <tr>
+          {map(itemHeaders, (header, headerKey) => {
+            if (isNumber(headerKey)) {
+              headerKey = header;
+            }
+            const isSortable = showSort && (size(sortableFields) === 0 || includes(sortableFields, headerKey));
+            const solSortOrder = headerKey === sortKey ? sortOrder : 'NONE';
+            return (
+              <th key={'header-' + kebabCase(headerKey)} data-sortable={isSortable} data-sort-key={headerKey}
                 data-sort-order={solSortOrder} onClick={() => this._onSortChange(headerKey, isSortable)}>
-              {header}
-              {isSortable && (<i className={classNames('pull-right', sortClasses['SORT_' + solSortOrder])}></i>)}
-            </th>
-          );
-        })}
-        <th className="action">
-        </th>
-      </tr>
+                {header}
+                {isSortable && (<i className={classNames('pull-right', sortClasses['SORT_' + solSortOrder])}></i>)}
+              </th>
+            );
+          })}
+          {showActionCol && (<th className="action"></th>)}
+        </tr>
       </thead>
     );
   }
 
   _renderRow(filteredItems) {
-    const {headers, onEditing, itemsPerPage = this.DEFAULT_PAGE_ITEM_COUNT, hidePagination, onDeleting, iconClasses, items} = this.props;
+    const { headers, onEditing, itemsPerPage = this.DEFAULT_PAGE_ITEM_COUNT, hidePagination, onDeleting, iconClasses, items, showActionCol } = this.props;
     const currentPage = !hidePagination ? this.state.currentPage : 0;
     const pagedItems = !hidePagination ? chunk(filteredItems, itemsPerPage) : [filteredItems];
     const icons = extend({
-      'DELETE': 'glyphicon glyphicon-trash',
-      'EDIT': 'glyphicon glyphicon-pencil',
-      'POPOVER_VIEW': 'glyphicon glyphicon-search'
+      'DELETE': 'fa fa-trash',
+      'EDIT': 'fa fa-pencil',
+      'POPOVER_VIEW': 'fa fa-search'
     }, iconClasses);
     const itemHeaders = size(headers) > 0 ? keys(headers) : keys(items[0]);
     return map(pagedItems[currentPage], (item, index) => {
-      const {popover} = item;
+      const { popover } = item;
       return (
-        <tr key={index}>
+        <tr key={index} onClick={() => this.props.onRowClick(item)} style={{ cursor: 'pointer' }}>
           {map(itemHeaders, (header) => <td key={'item-' + index + '-' + kebabCase(header)}>{item[header]}</td>)}
-          <td className="action-buttons">
-            {onEditing && <a onClick={() => this._onEditing(item, index)}><i className={icons.EDIT}/></a>}
-            {onDeleting && <a onClick={() => this._onDeleting(item, index)}><i className={icons.DELETE}/></a>}
-            {popover && <a data-toggle="popover" data-trigger="hover"
-                           data-placement="left" data-html="true" data-content={popover}>
-              <i className={icons.POPOVER_VIEW}/>
-            </a>}
-          </td>
+          {showActionCol && (
+            <td className="action-buttons">
+              {onEditing && <a onClick={() => this._onEditing(item, index)}><i className={icons.EDIT} /></a>}
+              {onDeleting && <a onClick={() => this._onDeleting(item, index)}><i className={icons.DELETE} /></a>}
+              {popover && <a data-toggle="popover" data-trigger="hover"
+                data-placement="left" data-html="true" data-content={popover}>
+                <i className={icons.POPOVER_VIEW} />
+              </a>}
+            </td>
+          )}
         </tr>
       );
     });
@@ -150,20 +161,20 @@ class DpDataTable extends Component {
 
   triggerPopover() {
     if (window && window.$ && window.$('.data-table-component a[data-toggle=popover]').popover) {
-      window.$('.data-table-component a[data-toggle=popover]').popover({container: 'body'});
+      window.$('.data-table-component a[data-toggle=popover]').popover({ container: 'body' });
     }
   }
 
   _filterItems() {
-    const {items, filterableFields, headers, onSorting, onFiltering} = this.props;
+    const { items, filterableFields, headers, onSorting, onFiltering } = this.props;
     const headerKeys = size(headers) > 0 ? keys(headers) : keys(items[0] || {});
-    const {sortKey, sortOrder} = this.state;
+    const { sortKey, sortOrder } = this.state;
     const filterString = this.state.filter || '';
     const filterKeys = (filterableFields && filterableFields.length) ? filterableFields : headerKeys;
     let processedItems = items;
     if (filterString !== '') {
       if (isFunction(onFiltering)) {
-        processedItems = onFiltering({items: processedItems, filterString});
+        processedItems = onFiltering({ items: processedItems, filterString });
       } else {
         processedItems = filter(items, (item) => {
           return some(filterKeys, (filterKey) => {
@@ -174,7 +185,7 @@ class DpDataTable extends Component {
     }
     if (sortKey !== '') {
       if (isFunction(onSorting)) {
-        processedItems = onSorting({items: processedItems, sortKey, sortOrder});
+        processedItems = onSorting({ items: processedItems, sortKey, sortOrder });
       } else {
         processedItems = sortBy(processedItems, [sortKey]);
         if (sortOrder === 'DESC') {
@@ -182,56 +193,57 @@ class DpDataTable extends Component {
         }
       }
     }
-    this._onItemsChange({items: processedItems});
+    this._onItemsChange({ items: processedItems });
     return processedItems;
   }
 
   _renderFilter() {
-    const {iconClasses} = this.props;
+    const { iconClasses } = this.props;
     const icons = extend({
-      'FILTER': 'glyphicon glyphicon-search'
+      'FILTER': 'fa fa-search'
     }, iconClasses);
-    return (<div className="form-group col-xs-12 col-sm-6 col-md-4">
+    return (<div className="form-group col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
       <label className="sr-only" htmlFor="filterTextInput">Filter text</label>
       <div className="input-group">
-        <div className="input-group-addon"><i className={icons.FILTER}/></div>
+        <div className="input-group-addon"><i className={icons.FILTER} /></div>
         <input type="text" className="form-control" id="filterTextInput" placeholder="Filter"
-               onChange={(e) => this._onFilterChange(e)}/>
+          onChange={(e) => this._onFilterChange(e)} />
       </div>
     </div>);
   }
 
   _renderHeaderPagination(items) {
-    const {itemsPerPage = this.DEFAULT_PAGE_ITEM_COUNT, showFilter, iconClasses} = this.props;
+    const { itemsPerPage = this.DEFAULT_PAGE_ITEM_COUNT, showFilter, iconClasses } = this.props;
     const currentPage = this.state.currentPage;
-    const paginationClass = classNames('form-group col-xs-12 col-sm-4 col-md-2', {
-      'col-sm-offset-2 col-md-offset-6': showFilter,
-      'col-sm-offset-8 col-md-offset-10': !showFilter
+    const paginationClass = classNames('form-group col-xs-12 col-sm-4 col-md-2 col-lg-2 col-xl-2', {
+      'offset-sm-2 offset-md-6 offset-lg-6 offset-xl-6': showFilter,
+      'offset-sm-8 offset-md-10 offset-lg-10 offset-xl-10': !showFilter
     });
     const icons = extend({
-      'PAGE_PREV': 'glyphicon glyphicon-backward',
-      'PAGE_NEXT': 'glyphicon glyphicon-forward'
+      'PAGE_PREV': 'fa fa-backward',
+      'PAGE_NEXT': 'fa fa-forward'
     }, iconClasses);
     const numberOfPages = ceil(items.length / itemsPerPage);
     const previousPage = currentPage > 0 ? currentPage - 1 : undefined;
     const nextPage = (currentPage + 1) < numberOfPages ? currentPage + 1 : undefined;
     return (<nav className={paginationClass}>
       <ul className="pagination pull-right">
-        <li className={classNames({'disabled': previousPage === undefined})}><a
+        <li className={classNames({ 'disabled': previousPage === undefined })}><a
           onClick={() => this._onPageClick(previousPage)}><i className={icons.PAGE_PREV}></i></a></li>
-        <li className={classNames({'disabled': nextPage === undefined})}><a
+          <li style={{marginTop: '7px'}}>&nbsp;&nbsp;<b>{currentPage + 1} / {numberOfPages}</b>&nbsp;&nbsp;</li>
+        <li className={classNames({ 'disabled': nextPage === undefined })}><a
           onClick={() => this._onPageClick(nextPage)}><i className={icons.PAGE_NEXT}></i></a></li>
       </ul>
     </nav>);
   }
 
   _renderFooterPagination(items) {
-    const {itemsPerPage = this.DEFAULT_PAGE_ITEM_COUNT} = this.props;
+    const { itemsPerPage = this.DEFAULT_PAGE_ITEM_COUNT } = this.props;
     const currentPage = this.state.currentPage;
     const numberOfPages = ceil(items.length / itemsPerPage);
-    return (<nav className="col-xs-12">
+    return (<nav className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
       <ul className="pagination pull-right">
-        {times(numberOfPages, (index) => <li key={index} className={classNames({'active': index === currentPage})}><a
+        {times(numberOfPages, (index) => <li key={index} className={classNames({ 'active': index === currentPage })}><a
           onClick={() => this._onPageClick(index)}>{index + 1}</a></li>)}
       </ul>
     </nav>);
@@ -239,45 +251,45 @@ class DpDataTable extends Component {
 
   _onPageClick(page) {
     if (page !== undefined) {
-      this.setState({currentPage: page, changeReason: 'PAGE_CHANGED'});
+      this.setState({ currentPage: page, changeReason: 'PAGE_CHANGED' });
     }
   }
 
   _onEditing(item, index) {
-    const {onEditing} = this.props;
-    this._onItemsChange({changeReason: 'ROW_EDITING', index, item});
+    const { onEditing } = this.props;
+    this._onItemsChange({ changeReason: 'ROW_EDITING', index, item });
     if (isFunction(onEditing)) {
-      onEditing({item, index});
+      onEditing({ item, index });
     }
   }
 
   _onDeleting(item, index) {
-    const {onDeleting} = this.props;
-    this._onItemsChange({changeReason: 'ROW_DELETING', index, item});
+    const { onDeleting } = this.props;
+    this._onItemsChange({ changeReason: 'ROW_DELETING', index, item });
     if (isFunction(onDeleting)) {
-      onDeleting({item, index});
+      onDeleting({ item, index });
     }
   }
 
   _onFilterChange(e) {
     const filterString = e.target.value;
-    this.setState({currentPage: 0, filter: filterString, changeReason: 'FILTER_CHANGED'});
+    this.setState({ currentPage: 0, filter: filterString, changeReason: 'FILTER_CHANGED' });
   }
 
   _onSortChange(headerKey, isSortable) {
     if (isSortable) {
-      const {sortKey, sortOrder} = this.state;
+      const { sortKey, sortOrder } = this.state;
       if (headerKey !== sortKey) {
-        this.setState({sortKey: headerKey, sortOrder: 'ASC', changeReason: 'SORT_CHANGED'});
+        this.setState({ sortKey: headerKey, sortOrder: 'ASC', changeReason: 'SORT_CHANGED' });
       } else {
         switch (sortOrder) {
           case 'NONE':
           case 'DESC':
-            this.setState({sortKey: headerKey, sortOrder: 'ASC', changeReason: 'SORT_CHANGED'});
+            this.setState({ sortKey: headerKey, sortOrder: 'ASC', changeReason: 'SORT_CHANGED' });
             break;
           case 'ASC':
           default:
-            this.setState({sortKey: headerKey, sortOrder: 'DESC', changeReason: 'SORT_CHANGED'});
+            this.setState({ sortKey: headerKey, sortOrder: 'DESC', changeReason: 'SORT_CHANGED' });
             break;
         }
       }
@@ -285,7 +297,7 @@ class DpDataTable extends Component {
   }
 
   _onItemsChange(options) {
-    const {onItemsChange} = this.props;
+    const { onItemsChange } = this.props;
     if (isFunction(onItemsChange)) {
       onItemsChange(extend({}, this.state, options));
     }
@@ -293,3 +305,4 @@ class DpDataTable extends Component {
 }
 
 export default DpDataTable;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 /* jshint node: true */
 var path = require('path');
+var webpack = require('webpack');
 var argv = require('yargs').argv;
 
 module.exports = {
   context: path.join(__dirname),
   entry: './src/index.js',
-
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    })
+  ],
   output: {
     path: path.join(__dirname, argv.ships == "dist" ? 'dist' : 'lib'),
     filename: 'dpDataTable.js',
@@ -19,15 +31,17 @@ module.exports = {
   } : {},
 
   module: {
-    loaders: [
-      {
+    loaders: [{
         test: /\.scss$/,
         // Query parameters are passed to node-sass
         loader: 'style!css!sass?outputStyle=expanded&' +
-        'includePaths[]=' + (path.resolve(__dirname, './bower_components')) + '&' +
-        'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
+          'includePaths[]=' + (path.resolve(__dirname, './bower_components')) + '&' +
+          'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
       },
-      {test: /\.json$/, loader: 'json-loader'},
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
       {
         test: /(\.js)|(\.jsx)$/,
         exclude: /node_modules/,
